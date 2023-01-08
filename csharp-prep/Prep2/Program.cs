fa551000230d01c74c1ffefc04557891f0e0facc
ivonnesue <ivonnesue12@gmail.com>
2023-01-07 18:25:41 -0800

    complete prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade); //convert answer to integer
+
+        
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (grade >=60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else 
+        {
+            Console.WriteLine("Your grade is F");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed");
+        }
+        
+        else
+        {
+            Console.WriteLine("You will do better next time.");
+        }
+
+    }   
 }
