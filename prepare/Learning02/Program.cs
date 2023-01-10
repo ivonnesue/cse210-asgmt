c4befa16c26978d8cbad21049e8a720739a71089
ivonnesue <ivonnesue12@gmail.com>
2023-01-09 22:52:03 -0800

    practiced adding a third person object

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
+        job1._jobTitle = "Graphic Designer";
         job1._company = "Microsoft";
         job1._startYear = 2019; //numbers in between " " are stored as string variables. Not needed at this point.
         job1._endYear = 2022;
@@ -16,12 +16,27 @@ class Program
         job2._startYear = 2022;
         job2._endYear = 2023;
 
+        //job 3 for person 2
+        Job job3 = new Job();
+        job3._jobTitle = "Graphic Designer";
+        job3._company = "PostNet";
+        job3._startYear = 2017;
+        job3._endYear = 2018;
+
         Resume myResume = new Resume();
         myResume._name = "Allison Rose";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
 
+        Resume myResume2 = new Resume();
+        myResume2._name = "Ivonne Ramos";
+       
+        myResume2._jobs.Add(job3);
+    
+
         myResume.Display();
+
+        myResume2.Display();
     }
 }
