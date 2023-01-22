a0862bb31c7d86d0ece3ebce19bfcf63bae36e2b
ivonnesue <ivonnesue12@gmail.com>
2023-01-21 16:06:36 -0800

    prove03, classes completed

---
+++ prove/Develop03/ScriptureMemorizer.cs
@@ -0,0 +1,59 @@
+using System;
+
+class ScriptureMemorizer
+{
+    private Scripture scripture;
+
+    private List<string> scritpureTextList;
+    
+    public ScritptureMemorizer(Scripture _scripture)
+    {
+        scripture = _scripture;
+        scritpureTextList = new List<string>();
+        convertTexttoList();
+    }
+
+    private void convertTexttoList()
+    {
+        scritpureTextList = scritpureTextList.ToString().Split(' ').ToList();
+    }
+
+    public void removeWordsFromText()
+    {
+        int numWordsToRemove = new Random().Next(3);
+        int wordsRemoved = 0;
+
+        do 
+        {
+            int randomIndex = new Random().Next(0, scritpureTextList.Count());
+            
+            scritpureTextList[randomIndex] = new string('_', scritpureTextList[randomIndex].Length);
+            wordsRemoved++;
+
+        }while (wordsRemoved != numWordsToRemove);
+
+    }
+    
+    public string toString()
+    {
+        return string.Join(' ', scritpureTextList);
+    }
+
+    public bool hasWordsLeft()
+    {
+        bool retvalue = false;
+        
+        foreach (string word in scritpureTextList)
+        {
+            if (word.Contains('_') == false)
+            {
+                retvalue = true;
+                break;
+            }
+        }
+
+        return retvalue;
+    }
+   
+
+}
