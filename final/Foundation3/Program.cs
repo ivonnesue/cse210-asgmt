5a9de4885953d3174aed4370c87763963d02d581
ivonnesue <ivonnesue12@gmail.com>
2023-02-13 22:56:52 -0800

    main program edit

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -11,7 +11,7 @@ class Program
         Console.WriteLine();
         
         //Event1
-        Lectures lecture = new Lectures("Les Brown", "Lecture",300);
+        Lectures lecture = new Lectures("Les Brown", "Lecture", 300);
         lecture.setEventTitle("Master the Mic");
         lecture.setDescription("Les Brown will bring out the world class" +
         "speaker in you, by showing you how to Master the Mic.");
